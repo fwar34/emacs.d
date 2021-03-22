71b59b298bf17982ef0bb0430ff6975d0073394d
fwar34 <fwar34@126.com>
2021-03-22 10:45:29 +0800

    update font size

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -158,7 +158,7 @@
    :font (font-spec :name "Sarasa Mono Slab K"
                     :weight 'normal
                     :slant 'normal
-                    :size 17))
+                    :size (if (string= "A12969" system-name) 17 20)))
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font
      (frame-parameter nil 'font)
