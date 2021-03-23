d17c50ba06379fe992fd058edb645597004974c4
fwar34 <fwar34@126.com>
2021-03-23 10:19:56 +0800

    update

---
+++ lisp/learn/my_test.el
@@ -0,0 +1,8 @@
+;; -*- coding: utf-8; lexical-binding: t; -*-
+(defun my-find-file-use-ivy ()
+  (interactive)
+  "My find file use ivy-read"
+  (let* ((cmd "find . -name *.*")
+         (shell-execute))
+    )
+  )
