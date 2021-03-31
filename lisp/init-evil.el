5d3d6b3c7a7748c0bcfdd6cf0874229a86cdf9c5
fwar34 <fwar34@126.com>
2021-03-31 17:14:11 +0800

    add C-u:delete whole line C-d:delete to the end line

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -126,6 +126,8 @@
 (define-key evil-normal-state-map (kbd "C-]") 'counsel-etags-find-tag-at-point)
 (define-key evil-insert-state-map (kbd "C-j") 'evil-next-line)
 (define-key evil-insert-state-map (kbd "C-k") 'evil-previous-line)
+(define-key evil-insert-state-map (kbd "C-u") 'evil-delete-whole-line)
+(define-key evil-insert-state-map (kbd "C-d") 'evil-delete-line)
 
 ;; evil-surround setting
 (global-evil-surround-mode)
