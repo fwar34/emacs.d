48038f314bc25649ea24b5d140ba80f81488c3eb
fwar34 <fwar34@126.com>
2021-04-06 16:47:49 +0800

    update

--- dump.el
+++ dump.el
@@ -11,91 +11,90 @@
 (setq evil-want-keybinding nil)
 ;; those we want manually
 (setq dump-packages '(
-                   company
-                   company-ctags
-                   company-statistics
-                   company-c-headers
-                   ;; company-english-helper
-                   ivy
-                   counsel
-                   general
-                   ;; use-package
-                   which-key 
-                   swiper
-                   winner
-                   elec-pair 
-                   rainbow-delimiters
-                   highlight-parentheses 
-                   savehist 
-                   expand-region
-                   magit
-                   ;;;
-                   undo-tree
-                   ;; evil
-                   ;; evil-collection
-                   hungry-delete
-                   transient
-                   ivy-xref
-                   ivy-rich
-                   js2-mode
-                   web-mode
-                   goto-chg
-                   ;;
-                   popwin
-                   winum
-                   ;; youdao-dictionary
-                   neotree
-                   ;; lispyville
-                   beacon
-                   symbol-overlay
-                   ;;;;
-                   rainbow-mode
-                   fix-word
-                   ;; browse-kill-ring
-                   markdown-mode
-                   highlight-numbers
-                   highlight-quoted
-                   highlight-defined
-                   highlight-parentheses
-                   imenu-list
-                   git-gutter
-                   diff-hl
-                   ;; taglist
-                   ;;;
-                   ace-popup-menu
-                   volatile-highlights
-                   dired
-                   dired-k
-                   dired-single
-                   ;; org
-                   lispy
-                   highlight-symbol
-                   ace-popup-menu
-                   multi-term
-                   thrift
-                   lua-mode
-                   cider
-                   inf-clojure
-                   god-mode
-                   ;;;;;
-                   indent-guide
-                   caps-lock
-                   perspective
-                   info-colors
-                   form-feed
-                   cc-mode
-                   ;;;;
-                   go-mode
-                   rust-mode
-                   unicad
-                   recentf
-                   hideshow
-                   isearch
-                   ediff
-                   ;;;;
-                   hydra
-                   zenburn-theme
-                   ))
+                      zenburn-theme
+                      company
+                      company-ctags
+                      company-statistics
+                      company-c-headers
+                      ;; company-english-helper
+                      ivy
+                      counsel
+                      general
+                      ;; use-package
+                      which-key 
+                      swiper
+                      winner
+                      elec-pair 
+                      rainbow-delimiters
+                      highlight-parentheses 
+                      savehist 
+                      expand-region
+                      magit
+                      ;;
+                      undo-tree
+                      ;; evil
+                      ;; evil-collection
+                      hungry-delete
+                      transient
+                      ivy-xref
+                      ivy-rich
+                      js2-mode
+                      web-mode
+                      goto-chg
+                      ;;
+                      popwin
+                      winum
+                      ;; youdao-dictionary
+                      neotree
+                      ;; lispyville
+                      beacon
+                      symbol-overlay
+                      ;;
+                      rainbow-mode
+                      fix-word
+                      ;; browse-kill-ring
+                      markdown-mode
+                      highlight-numbers
+                      highlight-quoted
+                      highlight-defined
+                      highlight-parentheses
+                      imenu-list
+                      git-gutter
+                      diff-hl
+                      ;; taglist
+                      ;;
+                      ace-popup-menu
+                      volatile-highlights
+                      dired
+                      dired-k
+                      dired-single
+                      ;; org
+                      lispy
+                      highlight-symbol
+                      ace-popup-menu
+                      multi-term
+                      thrift
+                      lua-mode
+                      cider
+                      inf-clojure
+                      god-mode
+                      ;;
+                      indent-guide
+                      caps-lock
+                      perspective
+                      info-colors
+                      form-feed
+                      cc-mode
+                      ;;
+                      go-mode
+                      rust-mode
+                      unicad
+                      recentf
+                      hideshow
+                      isearch
+                      ediff
+                      hydra
+                      ))
 
 (dolist (package dump-packages)
   (print package)
