48647364766c173316bc227d6ab21af16e7496c3
fwar34 <fwar34@126.com>
2021-04-13 17:21:58 +0800

    移出平滑滚动

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -42,18 +42,18 @@
 ;; https://github.com/MatthewZMD/.emacs.d/blob/master/README.md#smooth-scrolling
 ;; {{ 平滑滚动
 ;; Vertical Scroll
-(setq scroll-step 1)
-(setq scroll-margin 1)
-(setq scroll-conservatively 101)
-(setq scroll-up-aggressively 0.01)
-(setq scroll-down-aggressively 0.01)
-(setq auto-window-vscroll nil)
-(setq fast-but-imprecise-scrolling nil)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-(setq mouse-wheel-progressive-speed nil)
-;; Horizontal Scroll
-(setq hscroll-step 1)
-(setq hscroll-margin 1)
+;; (setq scroll-step 1)
+;; (setq scroll-margin 1)
+;; (setq scroll-conservatively 101)
+;; (setq scroll-up-aggressively 0.01)
+;; (setq scroll-down-aggressively 0.01)
+;; (setq auto-window-vscroll nil)
+;; (setq fast-but-imprecise-scrolling nil)
+;; (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+;; (setq mouse-wheel-progressive-speed nil)
+;; ;; Horizontal Scroll
+;; (setq hscroll-step 1)
+;; (setq hscroll-margin 1)
 ;; }}
 
 (provide 'init-ui)
