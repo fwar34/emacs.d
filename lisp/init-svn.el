406ac5b2f11e4fabf555c8e7cd78e3309ad3065b
fwar34 <fwar34@126.com>
2021-04-23 18:24:44 +0800

    test

--- lisp/init-svn.el
+++ lisp/init-svn.el
@@ -1,3 +1,11 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
+(defun svn-diff ()
+  (interactive)
+  (switch-to-buffer (get-buffer-create "*svn-diff*"))
+  (erase-buffer)
+  (evil-local-set-key 'normal (kbd "q") #'evil-buffer)
+  (start-process "my-make" "*svn-diff*" "svn" "diff")
+  (goto-char (point-min)))
+
 (provide 'init-svn)
