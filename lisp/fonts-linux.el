c8561b3ead634724fda1748c41f4485647415553
fwar34 <fwar34@126.com>
2021-10-23 05:06:58 +0000

    add fonts-linux.el

---
+++ lisp/fonts-linux.el
@@ -0,0 +1,61 @@
+;; linux 中 describe-char 打印的字体信息
+;; 中文的字体
+             position: 3125 of 64292 (5%), column: 11
+            character: 用 (displayed as 用) (codepoint 29992, #o72450, #x7528)
+              charset: unicode-bmp (Unicode Basic Multilingual Plane (U+0000..U+FFFF))
+code point in charset: 0x7528
+               script: han
+               syntax: w 	which means: word
+             category: .:Base, C:2-byte han, L:Left-to-right (strong), c:Chinese, h:Korean, j:Japanese, |:line breakable
+             to input: type "C-x 8 RET 7528"
+          buffer code: #xE7 #x94 #xA8
+            file code: #xE7 #x94 #xA8 (encoded by coding system utf-8-unix)
+              display: by this font (glyph code)
+    ftcrhb:-GOOG-Noto Sans CJK KR-normal-normal-normal-*-24-*-*-*-*-0-iso10646-1 (#x69AB)
+
+Character code properties: customize what to show
+  name: CJK IDEOGRAPH-7528
+  general-category: Lo (Letter, Other)
+  decomposition: (29992) ('用')
+
+There is an overlay here:
+ From 3117 to 3155
+  face                 hl-line
+  priority             -50
+  window               #<window 3 on init-packages.el>
+
+
+There are text properties here:
+  face                 font-lock-comment-face
+  fontified            t
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 英文的字体
+             position: 516 of 1168 (44%), column: 12
+            character: f (displayed as f) (codepoint 102, #o146, #x66)
+              charset: ascii (ASCII (ISO646 IRV))
+code point in charset: 0x66
+               script: latin
+               syntax: w 	which means: word
+             category: .:Base, L:Left-to-right (strong), a:ASCII, l:Latin, r:Roman
+             to input: type "C-x 8 RET 66" or "C-x 8 RET LATIN SMALL LETTER F"
+          buffer code: #x66
+            file code: #x66 (encoded by coding system utf-8-unix)
+              display: by this font (glyph code)
+    ftcrhb:-PfEd-DejaVu Sans Mono-normal-normal-normal-*-24-*-*-*-m-0-iso10646-1 (#x49)
+
+Character code properties: customize what to show
+  name: LATIN SMALL LETTER F
+  general-category: Ll (Letter, Lowercase)
+  decomposition: (102) ('f')
+
+There is an overlay here:
+ From 504 to 542
+  face                 hl-line
+  priority             -50
+  window               #<window 27 on fonts-linux.el>
+
+
+There are text properties here:
+  face                 (rainbow-identifiers-identifier-4)
+  fontified            nil
