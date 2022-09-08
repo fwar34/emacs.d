40c70766304e37d5c48a3fc3a0eacf38ef903e3c
fwar34 <fwar34@126.com>
2022-09-08 18:03:27 +0800

    update

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -5,25 +5,25 @@
 
 ;; {{ auto-save.el
 ;; reference from https://github.com/redguardtoo/emacs.d
-(with-eval-after-load 'evil
-  (defun local-require (pkg)
-    (unless (featurep pkg)
-      (load (expand-file-name
-             (cond
-              ((eq pkg 'go-mode-load)
-               (format "~/.emacs.d/site-lisp/go-mode/%s" pkg))
-              (t
-               (format "~/.emacs.d/site-lisp/%s/%s" pkg pkg))))
-            t t)))
-    (local-require 'auto-save)
-    ;; (add-to-list 'auto-save-exclude 'file-too-big-p t)
-    (add-to-list 'auto-save-exclude "COMMIT_EDITMSG")
-    (add-to-list 'auto-save-exclude "TAGS")
-    (add-to-list 'auto-save-exclude "tags")
-    (add-to-list 'auto-save-exclude "\\.cpp")
-    (setq auto-save-idle 2) ; 2 seconds
-    (setq auto-save-slient t)
-    (auto-save-enable))
+;; (with-eval-after-load 'evil
+;;   (defun local-require (pkg)
+;;     (unless (featurep pkg)
+;;       (load (expand-file-name
+;;              (cond
+;;               ((eq pkg 'go-mode-load)
+;;                (format "~/.emacs.d/site-lisp/go-mode/%s" pkg))
+;;               (t
+;;                (format "~/.emacs.d/site-lisp/%s/%s" pkg pkg))))
+;;             t t)))
+;;     (local-require 'auto-save)
+;;     ;; (add-to-list 'auto-save-exclude 'file-too-big-p t)
+;;     (add-to-list 'auto-save-exclude "COMMIT_EDITMSG")
+;;     (add-to-list 'auto-save-exclude "TAGS")
+;;     (add-to-list 'auto-save-exclude "tags")
+;;     (add-to-list 'auto-save-exclude "\\.cpp")
+;;     (setq auto-save-idle 2) ; 2 seconds
+;;     (setq auto-save-slient t)
+;;     (auto-save-enable))
 
 (use-package super-save
   :disabled
