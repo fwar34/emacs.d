c1d97530c278e64e13349abf379d6c7679b11d77
fwar34 <fwar34@126.com>
2023-03-18 22:08:04 +0800

    update

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -18,15 +18,14 @@
   :no-require t)
 
 ;; straight.el
-;; https://github.com/raxod502/straight.el#integration-with-use-package
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
