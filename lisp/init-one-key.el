06bddbe3c0a8c71c21c698036acd2b6452f327f7
fwar34 <fwar34@126.com>
2023-03-19 22:24:58 +0800

    update one-key

--- lisp/init-one-key.el
+++ lisp/init-one-key.el
@@ -1,19 +1,35 @@
 ;; -*- coding: utf-8; lexical-binding: t; -*-
 
 (use-package one-key
-  :load-path "lisp/one-key2.el"
+  :ensure t
+  :straight
+  (:host github :repo "manateelazycat/one-key")
   :bind
-  (("C-c C-o" . one-key-menu-test))
+  (("C-c C-o" . one-key-menu-magit))
   :config
-  (setq one-key-menu-test-alist
-        '(
-          ; (("n" . "evil normal state") . evil-normal-state)
-          ; (("i" . "evil insert state") . evil-insert-state)
-          ))
-  (defun one-key-menu-test ()
-    "The `one-key' menu for TEST."
+      (defun one-key-menu-magit ()
+    "The `one-key' menu for magit"
     (interactive)
-    (one-key-menu "TEST" one-key-menu-test-alist t))
+    (one-key-create-menu
+   "MAGIT"
+   '(
+     (("s" . "Magit status") . magit-status)
+     (("c" . "Magit checkout") . magit-checkout)
+     (("C" . "Magit commit") . magit-commit)
+     (("u" . "Magit push to remote") . magit-push-current-to-pushremote)
+     (("p" . "Magit delete remote branch") . magit-delete-remote-branch)
+     (("i" . "Magit pull") . magit-pull-from-upstream)
+     (("r" . "Magit rebase") . magit-rebase)
+     (("e" . "Magit merge") . magit-merge)
+     (("l" . "Magit log") . magit-log-all)
+     (("L" . "Magit blame") . magit-blame+)
+     (("b" . "Magit branch") . magit-branch)
+     (("B" . "Magit buffer") . magit-process-buffer)
+     (("D" . "Magit discarded") . magit-discard)
+     (("," . "Magit init") . magit-init)
+     (("." . "Magit add remote") . magit-remote-add)
+     )
+   t))
   )
 
 (provide 'init-one-key)
