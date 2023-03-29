de7ee06018321dbc4bae0653d162304034e50c84
fwar34 <fwar34@126.com>
2023-03-29 12:27:52 +0800

    update

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -11,6 +11,9 @@
   ;; (add-hook 'c++-mode-hook #'company-mode)
   ;; (add-hook 'c-mode-hook #'company-mode)
   :bind
+  (:map company-mode-map
+        ([remap completion-at-point] . company-complete)
+        )
   (:map company-active-map
         ("C-w" . backward-kill-word)
         ("C-u" . company-previous-page)
@@ -41,8 +44,8 @@
   ;; 注意： C-x = 用来检测光标下字符的数字，(insert 数字) 用来测试数字对应的字符。
   ;; (setq company-auto-commit-chars '(41 46 44 59 13))
 
-  ;; Number the candidates (use M-1, M-2 etc to select completions).
   ;; (setq company-show-numbers t)
+  ;; Number the candidates (use M-1, M-2 etc to select completions).
   (setq company-show-quick-access 'left)
   ;; make previous/next selection in the popup cycles
   (setq company-selection-wrap-around t)
@@ -50,13 +53,23 @@
   ;; so company should be case sensitive.
   ;; (setq company-dabbrev-ignore-case nil)
   (setq company-dabbrev-ignore-case t)
+  (setq company-files-exclusions '(".git/" ".svn/" ".DS_Store" "debian/"))
+    ;; No icons
+  (setq company-format-margin-function nil)
   ;; Trigger completion immediately.
-  ;; (setq company-idle-delay 0.3)
+  (setq company-idle-delay 0.1)
   ;; I don't like the downcase word in company-dabbrev!
   (setq company-dabbrev-downcase nil
         company-clang-insert-arguments nil
         company-require-match nil
         company-etags-ignore-case t)
+
+  ;; (company-backends '((company-capf :with company-tempo)
+  ;;                     company-files
+  ;;                     (company-dabbrev-code company-keywords)
+  ;;                     company-dabbrev
+  ;;                     ;; HACK: prevent `lsp-mode' to add `company-capf' back.
+  ;;                     company-capf))
 
   ;; {{{
   ;; 使用1-9来选择company补全选项, 0来使用company-filter-candidates(使用my-company-zero-key-for-filter来开关)
@@ -106,15 +119,15 @@ In that case, insert the number."
   ;;            (string-to-number k))))))
 
   ;; 0-9 来选择补全
-  (let ((map company-active-map))
-    (mapc (lambda (x) (define-key map (format "%d" x) 'ora-company-number))
-          (number-sequence 0 9))
-    ;; (define-key map " " (lambda ()
-    ;;                       (interactive)
-    ;;                       (company-abort)
-    ;;                       (self-insert-command 1)))
-    ;; (define-key map (kbd "<return>") nil)
-    )
+  ;; (let ((map company-active-map))
+  ;;   (mapc (lambda (x) (define-key map (format "%d" x) 'ora-company-number))
+  ;;         (number-sequence 0 9))
+  ;;   ;; (define-key map " " (lambda ()
+  ;;   ;;                       (interactive)
+  ;;   ;;                       (company-abort)
+  ;;   ;;                       (self-insert-command 1)))
+  ;;   ;; (define-key map (kbd "<return>") nil)
+  ;;   )
 
   ;; 自定义前面的补全数字提示
   ;; (defun ora--company-good-prefix-p (orig-fn prefix)
@@ -134,81 +147,82 @@ In that case, insert the number."
   ;; }}}
   )
 
-  (with-eval-after-load 'company
-    ;; (if (fboundp 'evil-declare-change-repeat)
-    ;;     (mapc 'evil-declare-change-repeat
-    ;;           '(company-complete-common
-    ;;             company-select-next
-    ;;             company-select-previous
-    ;;             company-complete-selection
-    ;;             company-complete-number)))
-    ;; 使用 c-n/c-p 来选择 company 的候选补全项
-    (define-key company-active-map (kbd "M-n") nil)
-    (define-key company-active-map (kbd "M-p") nil)
-    (define-key company-active-map (kbd "TAB") 'company-select-next-if-tooltip-visible-or-complete-selection)
-    (define-key company-active-map (kbd "C-n") 'company-select-next)
-    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-    ;; (add-to-list 'company-backends 'company-cmake)
-    ;; (add-to-list 'company-backends 'company-c-headers)
-    ;; can't work with TRAMP
-    (setq company-backends (delete 'company-ropemacs company-backends))
-    (setq company-backends (delete 'company-capf company-backends))
-    (setq company-backends (delete 'company-clang company-backends))
+(with-eval-after-load 'company
+  ;; (if (fboundp 'evil-declare-change-repeat)
+  ;;     (mapc 'evil-declare-change-repeat
+  ;;           '(company-complete-common
+  ;;             company-select-next
+  ;;             company-select-previous
+  ;;             company-complete-selection
+  ;;             company-complete-number)))
+  ;; 使用 c-n/c-p 来选择 company 的候选补全项
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "TAB") 'company-select-next-if-tooltip-visible-or-complete-selection)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  ;; (add-to-list 'company-backends 'company-cmake)
+  ;; (add-to-list 'company-backends 'company-c-headers)
+  ;; can't work with TRAMP
+  (setq company-backends (delete 'company-ropemacs company-backends))
+  (setq company-backends (delete 'company-capf company-backends))
+  (setq company-backends (delete 'company-clang company-backends))
 
 
-    ;; @see https://github.com/redguardtoo/emacs.d/commit/2ff305c1ddd7faff6dc9fa0869e39f1e9ed1182d
-    (defadvice company-in-string-or-comment (around company-in-string-or-comment-hack activate)
-      ;; you can use (ad-get-arg 0) and (ad-set-arg 0) to tweak the arguments
-      (if (memq major-mode '(php-mode html-mode web-mode nxml-mode))
-          (setq ad-return-value nil)
-        ad-do-it))
+  ;; @see https://github.com/redguardtoo/emacs.d/commit/2ff305c1ddd7faff6dc9fa0869e39f1e9ed1182d
+  (defadvice company-in-string-or-comment (around company-in-string-or-comment-hack activate)
+    ;; you can use (ad-get-arg 0) and (ad-set-arg 0) to tweak the arguments
+    (if (memq major-mode '(php-mode html-mode web-mode nxml-mode))
+        (setq ad-return-value nil)
+      ad-do-it))
 
-    ;; press SPACE will accept the highlighted candidate and insert a space
-    ;; `M-x describe-variable company-auto-complete-chars` for details
-    ;; That's BAD idea.
-    (setq company-auto-complete nil)
+  ;; press SPACE will accept the highlighted candidate and insert a space
+  ;; `M-x describe-variable company-auto-complete-chars` for details
+  ;; That's BAD idea.
+  (setq company-auto-complete nil)
 
-    ;; NOT to load company-mode for certain major modes.
-    ;; Ironic that I suggested this feature but I totally forgot it
-    ;; until two years later.
-    ;; https://github.com/company-mode/company-mode/issues/29
-    (setq company-global-modes
-          '(not
-            eshell-mode comint-mode erc-mode gud-mode rcirc-mode
-            minibuffer-inactive-mode)))
+  ;; NOT to load company-mode for certain major modes.
+  ;; Ironic that I suggested this feature but I totally forgot it
+  ;; until two years later.
+  ;; https://github.com/company-mode/company-mode/issues/29
+  (setq company-global-modes
+        '(not
+          eshell-mode comint-mode erc-mode gud-mode rcirc-mode
+          minibuffer-inactive-mode)))
 
-  (use-package company-tabnine
-    :disabled
-    :ensure t
-    :config
-    (add-to-list 'company-backends 'company-tabnine))
+(use-package company-tabnine
+  :disabled
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-tabnine))
 
-  ;; https://github.com/redguardtoo/company-ctags
-  (use-package company-ctags
-    :ensure t
-    :after company
-    :config
-    (setq company-ctags-extra-tags-files '("$HOME/TAGS" "/usr/include/c++/TAGS"))
-    (company-ctags-auto-setup)
+;; https://github.com/redguardtoo/company-ctags
+(use-package company-ctags
+  :ensure t
+  :after company
+  :config
+  ;; (setq company-ctags-extra-tags-files '("$HOME/TAGS" "/usr/include/c++/TAGS"))
+  (company-ctags-auto-setup)
 
-    ;; Use rusty-tags to generate tags file for Rust programming language.
-    ;; Add below code into ~/.emacs,
-    (setq company-ctags-tags-file-name "rusty-tags.emacs")
+  ;; Use rusty-tags to generate tags file for Rust programming language.
+  ;; Add below code into ~/.emacs,
+  (setq company-ctags-tags-file-name "rusty-tags.emacs")
 
-    ;; company-ctags-ignore-case
-    (setq company-ctags-ignore-case t))
+  ;; company-ctags-ignore-case
+  (setq company-ctags-ignore-case t))
 
-  ;; @see https://github.com/company-mode/company-mode/issues/348
-  (use-package company-statistics
-    :ensure t
-    :after company
-    :config
-    (company-statistics-mode))
+;; @see https://github.com/company-mode/company-mode/issues/348
+(use-package company-statistics
+  :disabled
+  :ensure t
+  :after company
+  :config
+  (company-statistics-mode))
 
-  (use-package company-c-headers
-    :ensure t
-    :config
-    (add-to-list 'company-backends 'company-c-headers))
+(use-package company-c-headers
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-c-headers))
 
 (provide 'init-company)
 ;;; init-company.el ends here
